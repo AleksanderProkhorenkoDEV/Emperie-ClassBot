65bd7f6cf8e53df37cc7a7470bd4a69993f60b45
Aleksander <aprokhorenkodev@gmail.com>
2025-07-14 17:36:01 +0200

    feat: create method to obtain traslates

--- app/src/index.ts
+++ app/src/index.ts
@@ -1,4 +1,5 @@
 import TelegramBot from "node-telegram-bot-api";
+import fs from 'node:fs';
 
 // Verifica que el token esté definido
 if (!process.env.TELEGRAM_TOKEN) {
@@ -8,9 +9,32 @@ if (!process.env.TELEGRAM_TOKEN) {
 // Inicializa el bot
 const bot = new TelegramBot(process.env.TELEGRAM_TOKEN, { polling: true });
 
+bot.on("message", (msg) => {
+    const lang = msg.from.language_code;
+    const name = msg.from.first_name;
+    const chatId = msg.chat.id
+
+    const t = traslate(lang, "welcome");
+
+    const message = `${t.title.replace(/{{name}}/g, name)}\n\n${t.description}`;
+    bot.sendMessage(chatId,message,{ parse_mode: 'Markdown' })
+
+});
+
 // Manejo de errores
 bot.on("polling_error", (error) => {
     console.error(`⚠️ Error en el bot: ${error.message}`);
 });
 
 console.log("✅ Bot activo. Usa /start para probar.");
+
+const loadTraslation = (languajeCode: string = 'es') => {
+    const filePath = `./lang/${languajeCode}.json`
+    const data = fs.readFileSync(filePath, 'utf8');
+    return JSON.parse(data);
+}
+
+const traslate = (languajeCode: string = "es", section: string) => {
+    const file = loadTraslation(languajeCode);
+    return file[section] as Record<string, string>;
+}
