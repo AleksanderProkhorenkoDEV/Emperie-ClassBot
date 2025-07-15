e01746eb10735e8a0956347a0f9d0db22bba937e
aleksander <aprokhorenkodev@gmail.com>
2025-07-15 19:28:09 +0200

    refactor: only display the start message when the user write /start and make a new comand

--- app/src/index.ts
+++ app/src/index.ts
@@ -1,40 +1,40 @@
 import TelegramBot from "node-telegram-bot-api";
-import fs from 'node:fs';
+import { traslate } from "./i18n";
+import fs from "node:fs";
 
 // Verifica que el token esté definido
 if (!process.env.TELEGRAM_TOKEN) {
-    throw new Error("❌ Falta el token de Telegram. Añádelo en .env");
+  throw new Error("❌ Falta el token de Telegram. Añádelo en .env");
 }
 
 // Inicializa el bot
 const bot = new TelegramBot(process.env.TELEGRAM_TOKEN, { polling: true });
 
-bot.on("message", (msg) => {
-    const lang = msg.from.language_code;
-    const name = msg.from.first_name;
-    const chatId = msg.chat.id
+bot.onText(/\/start/, (msg) => {
+  const lang = msg.from.language_code;
+  const name = msg.from.first_name;
+  const chatId = msg.chat.id;
 
-    const t = traslate(lang, "welcome");
+  const t = traslate(lang, "welcome");
 
-    const message = `${t.title.replace(/{{name}}/g, name)}\n\n${t.description}`;
-    bot.sendMessage(chatId,message,{ parse_mode: 'Markdown' })
+  const message = `${t.title.replace(/{{name}}/g, name)}\n\n${t.description}`;
+  bot.sendMessage(chatId, message, { parse_mode: "HTML" });
+});
 
+bot.onText(/\/cursos/, (msg) => {
+  const chatId = msg.chat.id;
+  const lang = msg.from.language_code;
+
+  const t = traslate(lang, "stampingNailMaraton");
+  const message = `${t.title}\n\n${t.summary}`;
+
+  bot.sendMessage(chatId, message, { parse_mode: "HTML" });
+  bot.sendPhoto(chatId, "https://res.cloudinary.com/dvrcz4v6x/image/upload/v1752599470/bot-emperie_kpa5ni.jpg");
 });
 
 // Manejo de errores
 bot.on("polling_error", (error) => {
-    console.error(`⚠️ Error en el bot: ${error.message}`);
+  console.error(`⚠️ Error en el bot: ${error.message}`);
 });
 
 console.log("✅ Bot activo. Usa /start para probar.");
-
-const loadTraslation = (languajeCode: string = 'es') => {
-    const filePath = `./lang/${languajeCode}.json`
-    const data = fs.readFileSync(filePath, 'utf8');
-    return JSON.parse(data);
-}
-
-const traslate = (languajeCode: string = "es", section: string) => {
-    const file = loadTraslation(languajeCode);
-    return file[section] as Record<string, string>;
-}
