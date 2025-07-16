191a061b1f7cdc2e105534bd6d5bdc4168f19d86
aleksander <aprokhorenkodev@gmail.com>
2025-07-16 16:47:43 +0200

    fix: change the import url of stamping function

--- app/src/commands/index.ts
+++ app/src/commands/index.ts
@@ -1,5 +1,5 @@
 import TelegramBot from "node-telegram-bot-api";
-import { stamping } from "./stamping";
+import { stamping } from "./stamping/stamping";
 import { start } from "./start";
 
 export const setUpBotCommands = (bot: TelegramBot) => {
