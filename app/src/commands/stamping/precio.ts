bbad56bd8d9c7db663f974f29bd8a56b882a016c
aleksander <aprokhorenkodev@gmail.com>
2025-07-16 17:13:37 +0200

    feat: display a qr with the user name of Olga in price section

--- app/src/commands/stamping/precio.ts
+++ app/src/commands/stamping/precio.ts
@@ -1,5 +1,6 @@
 import TelegramBot from "node-telegram-bot-api";
 import { traslate } from "../../i18n";
+import fs from "node:fs";
 
 export const stampingPrince = (bot: TelegramBot) => {
   bot.onText(/\/precio/, (msg) => {
@@ -10,5 +11,16 @@ export const stampingPrince = (bot: TelegramBot) => {
     const message = `${t.title}\n\n${t.summary}\n\n${t.details}\n\n${t.stampingDate}\n\n${t.stampingRegister}`;
 
     bot.sendMessage(chatId, message, { parse_mode: "HTML" });
+
+    const buffer = fs.readFileSync("/app/public/images/qr-bot.png");
+    bot.sendPhoto(
+      chatId,
+      buffer,
+      {},
+      {
+        filename: "QR_Olga_Prokhorenko.jpg",
+        contentType: "image/png",
+      }
+    );
   });
 };
