d29eb4d4b66b32f4ff53d870b032719f255412dc
aleksander <aprokhorenkodev@gmail.com>
2025-07-16 17:06:45 +0200

    feat: call the new command

--- app/src/commands/index.ts
+++ app/src/commands/index.ts
@@ -1,8 +1,10 @@
 import TelegramBot from "node-telegram-bot-api";
 import { stamping } from "./stamping/stamping";
 import { start } from "./start";
+import { ayuda } from "./ayuda";
 
 export const setUpBotCommands = (bot: TelegramBot) => {
   start(bot);
   stamping(bot);
+  ayuda(bot)
 };
